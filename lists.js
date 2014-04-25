a664cd5e3aee61588b148bfc794477dbcdae1063
Tom Sabin <tom.sabin@unboxedconsulting.com>
2014-04-25 11:45:08 +0100

    Use collection hooks to determine position of new list item

--- lists.js
+++ lists.js
@@ -1,5 +1,20 @@
 Items = new Meteor.Collection('items');
 
+Items.before.insert(function (userId, doc) {
+  var position, highestItem;
+  highestItem = Items.findOne({},
+                     { sort: { position: -1 },
+                     limit: 1});
+
+  if (typeof highestItem !== "undefined" && highestItem !== null) {
+    position = highestItem.position;
+  } else {
+    position = 0;
+  }
+
+  doc.position = position + 1;
+});
+
 if (Meteor.isClient) {
   Template.items.items = function () {
     return Items.find({}, {sort: {'position': 1}});
@@ -7,19 +22,19 @@ if (Meteor.isClient) {
 
   Template.items.events({
     'click input': function () {
-      console.log('You pressed the button');
+      Items.insert({ body: 'A new item' });
     }
   });
 }
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
-    if (Items.find().count() === 0) {
-      _.each(
-        ['One', 'Two', 'Three'], function (body, index) {
-          Items.insert({ body: body, position: index + 1 });
-        }
-      );
-    }
+    // if (Items.find().count() === 0) {
+    //   _.each(
+    //     ['One', 'Two', 'Three'], function (body, index) {
+    //       Items.insert({ body: body, position: index + 1 });
+    //     }
+    //   );
+    // }
   });
 }
